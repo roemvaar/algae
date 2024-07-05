8b7ffbfca0b8a7ce67563878828ec80d7cefb52c
Roberto Valenzuela <valenzuelarober@gmail.com>
2024-07-05 11:17:13 -0400

    queue: create and destroy methods

--- include/queue.h
+++ include/queue.h
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-typedef struct {
+#define QUEUE_ERROR_NULLPTR "[QUEUE]: Error, NULL Pointer"
+#define QUEUE_ERROR_MALLOC "[QUEUE]: Error, Unable to allocate memory"
+
+typedef struct
+{
     bool full;
     bool empty;
     size_t tail;
@@ -17,3 +22,6 @@ queue queue_destroy(queue **q);
 bool queue_push(queue *q, void *element);
 void *queue_pop(queue *q);
 void *queue_front(queue *q);
+bool queue_empty(queue *q);
+bool queue_full(queue *q);
+bool check_nullptr(void *q, char *error);
---
+++ src/queue.c
@@ -0,0 +1,61 @@
+#include "queue.h"
+
+queue *queue_create(size_t size, size_t element)
+{
+    queue *q;
+
+    q = malloc(sizeof(queue));
+
+    if (check_nullptr(q, QUEUE_ERROR_MALLOC)) {
+        return NULL;
+    }
+
+    q->elements = malloc(size * sizeof(void *));
+
+    if (check_nullptr(q->elements, QUEUE_ERROR_MALLOC)) {
+        free(q);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+        q->elements[i] = malloc(element_size);
+
+        if (check_nullptr(q->elements[i], QUEUE_ERROR_MALLOC)) {
+            free(q);
+            free(q->elements);
+
+            for (size_t j = 0; j < i; j++) {
+                free(q->elements[j]);
+            }
+        }
+    }
+
+    q->head = 0;
+    q->tail = 0;
+    q->full = false;
+    q->empty = true;
+    q->element_size = element_size;
+    q->size = size;
+
+    return q;
+}
+
+void queue_destroy(queue **q)
+{
+    if (check_nullptr(q, QUEUE_ERROR_NULLPTR)) {
+        return;
+    }
+
+    if (check_nullptr(*q, QUEUE_ERROR_NULLPTR)) {
+        return;
+    }
+
+    for (size_t i = 0; i < (*q)->size; i++) {
+        free(*((*q)->elements+i));
+    }
+
+    free((*q)->elements);
+    free(*q);
+
+    *q = NULL;
+}
